17923411f17d53c4de6bc8d9285cfc8ca648465e
daniel <daniel.monk@outlook.com>
2026-02-21 16:36:38 +0000

    feat: add Patient model

---
+++ CodeIgnitor/app/Models/PatientModel.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+class PatientModel extends Model {
+    protected $table = 'patients';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['practice_id', 'name', 'email', 'phone', 'date_of_birth'];
+    protected $useTimestamps = true;
+}
