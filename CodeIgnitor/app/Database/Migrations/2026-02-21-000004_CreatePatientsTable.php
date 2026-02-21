95f58eae026e114f05ea86ec8d13ed4bdac4febe
daniel <daniel.monk@outlook.com>
2026-02-21 17:28:14 +0000

    feat: create patients migration

---
+++ CodeIgnitor/app/Database/Migrations/2026-02-21-000004_CreatePatientsTable.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Database\Migrations;
+use CodeIgniter\Database\Migration;
+
+class CreatePatientsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'            => ['type' => 'INT', 'auto_increment' => true],
+            'practice_id'   => ['type' => 'INT'],
+            'name'          => ['type' => 'VARCHAR', 'constraint' => 255],
+            'email'         => ['type' => 'VARCHAR', 'constraint' => 255],
+            'phone'         => ['type' => 'VARCHAR', 'constraint' => 255],
+            'date_of_birth' => ['type' => 'DATE', 'null' => true],
+            'created_at'    => ['type' => 'DATETIME', 'null' => true],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->addForeignKey('practice_id', 'practices', 'id');
+        $this->forge->createTable('patients');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('patients');
+    }
+}
