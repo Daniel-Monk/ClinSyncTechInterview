fc676e3758cd44aa633e303c43dd618c0cfe442c
daniel <daniel.monk@outlook.com>
2026-02-21 17:24:08 +0000

    feat: create tokens migration

---
+++ CodeIgnitor/app/Database/Migrations/2026-02-21-000003_CreateTokensTable.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Database\Migrations;
+use CodeIgniter\Database\Migration;
+
+class CreateTokensTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'            => ['type' => 'INT', 'auto_increment' => true],
+            'user_id'       => ['type' => 'INT'],
+            'practice_id'   => ['type' => 'INT'],
+            'token'         => ['type' => 'VARCHAR', 'constraint' => 64],
+            'expires_at'    => ['type' => 'DATETIME', 'null' => true],
+            'revoked_at'    => ['type' => 'DATETIME', 'null' => true],
+            'created_at'    => ['type' => 'DATETIME', 'null' => true],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->addForeignKey('user_id', 'users', 'id');
+        $this->forge->addForeignKey('practice_id', 'practices', 'id');
+        $this->forge->createTable('tokens');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tokens');
+    }
+}
