ac45fd1e3382462f91521c3ed4313dee2a320756
daniel <daniel.monk@outlook.com>
2026-02-21 17:03:45 +0000

    feat: add practices table migration

---
+++ CodeIgnitor/app/Database/Migrations/2026-02-21-000001_CreatePracticesTable.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Database\Migrations;
+use CodeIgniter\Database\Migration;
+
+class CreatePracticesTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'            => ['type' => 'INT', 'auto_increment' => true],
+            'name'          => ['type' => 'VARCHAR', 'constraint' => 255],
+            'created_at'    => ['type' => 'DATETIME', 'null' => true],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('practices');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('practices');
+    }
+}
