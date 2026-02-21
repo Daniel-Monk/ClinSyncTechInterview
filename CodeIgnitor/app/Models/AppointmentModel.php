7de915bed14f4a80b17fa5f4265aabf92df4e6e6
daniel <daniel.monk@outlook.com>
2026-02-21 16:36:28 +0000

    feat: add Appointment model

---
+++ CodeIgnitor/app/Models/AppointmentModel.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+class AppointmentModel extends Model{
+    protected $table = 'appointments';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['practice_id', 'patient_id', 'user_id', 'appointment_at', 'notes'];
+    protected $useTimestamps = true;
+}
