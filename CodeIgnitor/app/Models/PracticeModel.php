707e72e20e86a99bd95366f90c9c88e42e3a5ebc
daniel <daniel.monk@outlook.com>
2026-02-21 16:36:46 +0000

    feat: add Practice model

---
+++ CodeIgnitor/app/Models/PracticeModel.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+class PracticeModel extends Model
+{
+    protected $table = 'practices';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['name'];
+    protected $useTimestamps = true;
+}
