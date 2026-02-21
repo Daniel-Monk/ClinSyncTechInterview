e53eecdd6a583553ecfdcbbf79a930ccc6ba9dd1
daniel <daniel.monk@outlook.com>
2026-02-21 16:45:19 +0000

    fix: consistent formatting across models

--- CodeIgnitor/app/Models/AppointmentModel.php
+++ CodeIgnitor/app/Models/AppointmentModel.php
@@ -2,7 +2,8 @@
 namespace App\Models;
 use CodeIgniter\Model;
 
-class AppointmentModel extends Model{
+class AppointmentModel extends Model
+{
     protected $table = 'appointments';
     protected $primaryKey = 'id';
     protected $allowedFields = ['practice_id', 'patient_id', 'user_id', 'appointment_at', 'notes'];
--- CodeIgnitor/app/Models/PatientModel.php
+++ CodeIgnitor/app/Models/PatientModel.php
@@ -2,7 +2,8 @@
 namespace App\Models;
 use CodeIgniter\Model;
 
-class PatientModel extends Model {
+class PatientModel extends Model 
+{
     protected $table = 'patients';
     protected $primaryKey = 'id';
     protected $allowedFields = ['practice_id', 'name', 'email', 'phone', 'date_of_birth'];
