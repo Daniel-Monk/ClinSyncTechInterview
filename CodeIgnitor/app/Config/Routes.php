10d2828eb64f81808e14577de955aaee3891d8af
daniel <daniel.monk@outlook.com>
2026-02-22 18:44:46 +0000

    feat: add appointment routes

--- CodeIgnitor/app/Config/Routes.php
+++ CodeIgnitor/app/Config/Routes.php
@@ -7,7 +7,7 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
-$routes->post('api/login','Api\AuthController::login');
+$routes->post('api/login', 'Api\AuthController::login');
 
 $routes->group('api', static function ($routes) {
     $routes->get('health', 'Api\Health::index');
@@ -16,4 +16,6 @@ $routes->group('api', static function ($routes) {
 $routes->group('api', ['filter' => 'auth'], static function ($routes) {
     $routes->get('patients', 'Api\PatientController::index');
     $routes->post('patients', 'Api\PatientController::create');
+    $routes->get('appointments', 'Api\AppointmentController::index');
+    $routes->post('appointments', 'Api\AppointmentController::create');
 });
